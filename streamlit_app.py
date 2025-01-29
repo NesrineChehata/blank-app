b308745a27a33971ef858fc2fcbe358dfb6a423d
Nesrine Chehata <75976309+NesrineChehata@users.noreply.github.com>
2025-01-29 22:57:23 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
 
-st.title("🎈 ABG app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Exemple d'application avec Streamlit")
+
+x = np.linspace(0, 10, 100)
+a = st.slider("Choisissez un coefficient", 0.0, 5.0, 1.0)
+y = a * np.sin(x)
+
+fig, ax = plt.subplots()
+ax.plot(x, y)
+st.pyplot(fig)
